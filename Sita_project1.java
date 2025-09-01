51ec0231e21fc35078d4499c70852e6e9917c473
Sita <ariantishita@gmail.com>
2025-09-01 13:55:19 +0700

    Add files via upload

---
+++ Sita_project1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sita_project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+
+public class Sita_project1 {
+    public static void main(String[] args) {
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("Nama   : Sita");
+        System.out.println("NPM    : 2410506014");
+        System.out.println("Alamat : Jl. Sunan Bonang IV");
+        System.out.println("No HP  : 081234567890");
+    }
+}
